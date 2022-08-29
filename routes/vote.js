fe75a05ac47d6c64ec283cdf89164100debbe9ae
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-08-29 17:32:03 +0900

    선택지, 투표 기능 구현 거의 완료

--- routes/vote.js
+++ routes/vote.js
@@ -15,17 +15,18 @@ router.post('/:selectKey', AuthMiddleware, async (req, res) => {
     const data = await Select.findOne({ where: { selectKey } });
 
     if (!data) {
-      res.status(400).json({
+      return res.status(400).json({
         ok: false,
         errMsg: '해당 선택글이 존재하지 않음',
       });
-      return;
     }
 
+    // 투표했는지 확인
     const voteCheck = await Vote.findOne({
       where: { selectKey, userKey },
     });
 
+    // 안하면 투표 데이터 생성
     if (!voteCheck) {
       await Vote.create({
         selectKey,
@@ -33,6 +34,7 @@ router.post('/:selectKey', AuthMiddleware, async (req, res) => {
         choice,
       });
     } else {
+      // 했다면 수정(수정은 원래 예정에 없는건데 일단 만듬)
       await Vote.update(
         { choice },
         {
@@ -41,109 +43,105 @@ router.post('/:selectKey', AuthMiddleware, async (req, res) => {
       );
     }
 
-    res.status(200).json({
+    // 일단은 투표하면 그냥 메세지만 응답하고, 새로 get요청 받을생각
+    return res.status(200).json({
       ok: true,
       msg: '선택지 투표 성공',
     });
-    return;
   } catch (err) {
     console.log(err);
-    res.status(500).json({
+    return res.status(500).json({
       ok: false,
       errMsg: '선택지 투표 실패.',
     });
-    return;
   }
 });
 
 // 선택지 비율 조회
-router.get('/:selectKey', async (req, res) => {
+router.get('/:selectKey', isLoginMiddlware, async (req, res) => {
   try {
     const { selectKey } = req.params;
 
     const isSelect = await Select.findOne({ where: { selectKey } });
 
     if (!isSelect) {
-      res.status(400).json({
+      return res.status(400).json({
         ok: false,
         errMsg: '해당 선택글이 존재하지 않음',
       });
-      return;
     }
 
-    // const user = res.locals.user;
-    // console.log('잉?' + user.userKey);
-    // if (!user) {
-    //   res.status(400).json({
-    //     ok: false,
-    //     errMsg: '비로그인 사람임',
-    //   });
-    //   return;
-    // }
-
-    // const userKey = user.userKey;
-    const userKey = 22;
-
-    const voteCheck = await Vote.findOne({
-      where: { selectKey, userKey },
-    });
-
-    // false로 처리하는게 좋은지 아님 빈값을 주는게 맞는지..
-    // 아니면 총 참여자 수만 줄까?
-    if (!voteCheck) {
-      res.status(400).json({
+    const user = res.locals.user;
+    // 미들웨어를 거쳐서 로그인 유무 확인(비로그인시)
+    if (!user) {
+      return res.status(400).json({
         ok: false,
-        errMsg: '참여자가 투표를 하지 않음',
+        errMsg: '비로그인 상태',
       });
-      return;
-    }
+    } else {
+      // 미들웨어를 거쳐서 로그인 유무 확인(로그인시)
+      const userKey = user.userKey;
 
-    const datas = await Vote.findAll({
-      where: { selectKey },
-    });
+      const voteCheck = await Vote.findOne({
+        where: { selectKey, userKey },
+      });
 
-    let count1 = 0;
-    let count2 = 0;
-    let count3 = 0;
-    let count4 = 0;
-    datas.map((e) => {
-      if (e.choice === 1) {
-        ++count1;
-      } else if (e.choice === 2) {
-        ++count2;
-      } else if (e.choice === 3) {
-        ++count3;
-      } else if (e.choice === 4) {
-        ++count4;
+      // 로그인은 했지만, 투표를 안하면 비율 안보이게함
+      // false로 처리하는게 좋은지 아님 빈값을 주는게 맞는지..
+      // 아니면 총 참여자 수만 줄까?
+      if (!voteCheck) {
+        return res.status(200).json({
+          ok: false,
+          errMsg: '참여자가 투표를 하지 않음',
+        });
+      } else {
+        // 로그인하고 투표까지하면 투표비율 보여줌
+        const datas = await Vote.findAll({
+          where: { selectKey },
+        });
+
+        let count1 = 0;
+        let count2 = 0;
+        let count3 = 0;
+        let count4 = 0;
+        datas.map((e) => {
+          if (e.choice === 1) {
+            ++count1;
+          } else if (e.choice === 2) {
+            ++count2;
+          } else if (e.choice === 3) {
+            ++count3;
+          } else if (e.choice === 4) {
+            ++count4;
+          }
+        });
+        let total = count1 + count2 + count3 + count4;
+
+        const isVote = await Vote.findOne({
+          where: { selectKey, userKey },
+          attributes: ['choice'],
+        });
+
+        return res.status(200).json({
+          ok: true,
+          msg: '선택지 비율 조회 성공',
+          result: {
+            1: (Math.round((count1 / total) * 100) / 100) * 100,
+            2: (Math.round((count2 / total) * 100) / 100) * 100,
+            3: (Math.round((count3 / total) * 100) / 100) * 100,
+            4: (Math.round((count4 / total) * 100) / 100) * 100,
+            total,
+            isVote: isVote.choice,
+          },
+        });
       }
-    });
-    console.log(count1, count2, count3, count4);
-    let total = count1 + count2 + count3 + count4;
-
-    const isVote = await Vote.findOne({
-      where: { selectKey, userKey },
-      attributes: ['choice'],
-    });
-
-    res.status(200).json({
-      ok: true,
-      msg: '선택지 비율 조회 성공',
-      result: {
-        1: (Math.round((count1 / total) * 100) / 100) * 100,
-        2: (Math.round((count2 / total) * 100) / 100) * 100,
-        3: (Math.round((count3 / total) * 100) / 100) * 100,
-        4: (Math.round((count4 / total) * 100) / 100) * 100,
-        total,
-        isVote: isVote.choice,
-      },
-    });
+    }
   } catch (err) {
     console.log(err);
-    res.status(500).json({
+    return res.status(500).json({
       ok: false,
       errMsg: '선택지 비율 조회 실패.',
     });
-    return;
   }
 });
 module.exports = router;
