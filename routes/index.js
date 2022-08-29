82b09b9ecf97c731f9c9273ebb0eb2d834445317
JJooonji <korewa9@naver.com>
2022-08-29 13:42:40 +0900

    유저 기능 1차 코드 작성 완료

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const selectRouter = require('./select');
+const userRouter = require("./user")
 
 router.use('/select', selectRouter);
+router.use("/", userRouter);
 
 module.exports = router;
