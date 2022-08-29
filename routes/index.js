a2a6d4bc6ddbeca465bcbe6e8524089dd361a701
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-08-29 14:24:22 +0900

    투표 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const selectRouter = require('./select');
+const voteRouter = require('./vote');
 
 router.use('/select', selectRouter);
-
+router.use('/select/vote', voteRouter);
 module.exports = router;
