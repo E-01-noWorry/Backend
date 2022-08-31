50e41fbab16e447543ba31cc17778f607d92d7d5
김대린 <62937325+kimdaerin@users.noreply.github.com>
2022-08-31 15:22:24 +0900

    채팅

--- migrations/20220827092058-create-chat.js
+++ migrations/20220827092058-create-chat.js
@@ -1,12 +1,42 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Chats', {
-      chatId: {
+    await queryInterface.createTable('Rooms', {
+      roomId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      hostId: {
+        type: Sequelize.STRING,
+      },
+      hostNickname: {
+        type: Sequelize.STRING,
+      },
+      hostImg: {
+        type: Sequelize.STRING,
+      },
+      max: {
+        type: Sequelize.STRING,
+      },
+      hashTag: {
+        type: Sequelize.JSON,
+      },
+      roomUserId: {
+        type: Sequelize.JSON,
+      },
+      roomUserNickname: {
+        type: Sequelize.JSON,
+      },
+      roomUserNum: {
+        type: Sequelize.INTEGER,
+      },
+      roomUserImg: {
+        type: Sequelize.JSON,
       },
       createdAt: {
         allowNull: false,
@@ -19,6 +49,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Chats');
+    await queryInterface.dropTable('Rooms');
   },
 };
