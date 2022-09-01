2b259a05cbdd5e976853e96852c5c441fc3eb7f3
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-09-01 11:09:54 +0900

    db 변경 채팅방 crud 작성중

--- models/participant.js
+++ models/participant.js
@@ -1,38 +1,30 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Room extends Model {
+  class Participant extends Model {
     static associate(models) {
       this.belongsTo(models.User, {
         foreignKey: 'userKey',
         targetKey: 'userKey',
       });
-      this.hasMany(models.Chat, {
+      this.belongsTo(models.Room, {
         foreignKey: 'roomKey',
-        sourceKey: 'roomKey',
-      });
-      this.hasMany(models.Participant, {
-        foreignKey: 'roomKey',
-        sourceKey: 'roomKey',
+        targetKey: 'roomKey',
       });
     }
   }
-  Room.init(
+  Participant.init(
     {
-      roomKey: {
+      participantKey: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true,
       },
-      title: DataTypes.STRING,
-      max: DataTypes.INTEGER,
-      hashTag: DataTypes.JSON,
     },
     {
-      timestamp: true,
       sequelize,
-      modelName: 'Room',
+      modelName: 'Participant',
     }
   );
-  return Room;
+  return Participant;
 };
