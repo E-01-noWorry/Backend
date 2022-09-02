97e85dfe878b836037e383b17c1d328ee96d12b8
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-09-02 15:13:00 +0900

    로그 남기기 적용 완료

--- advice/errorHandler.js
+++ advice/errorHandler.js
@@ -1,9 +1,9 @@
+const logger = require('../config/winston');
 const ErrorCustom = require('./errorCustom');
-// const logger = require('../config/logger');
 
 module.exports = (err, req, res, next) => {
-  // logger.error('err');
   console.log(err);
+  logger.error(err);
   if (err instanceof ErrorCustom) {
     return res.status(err.code).json({ ok: false, errMsg: err.message });
   }
--- config/winston.js
+++ config/winston.js
@@ -1,27 +1,26 @@
 const winston = require('winston');
 const winstonDaily = require('winston-daily-rotate-file');
-// const process = require('process');
-
-const { combine, timestamp, label, printf } = winston.format;
 
 //* 로그 파일 저장 경로 → 루트 경로/logs 폴더
 const logDir = './config/logs';
 require('dotenv').config();
 
+const { combine, timestamp, label, printf } = winston.format;
+
 //* log 출력 포맷 정의 함수
 const logFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`; // 날짜 [시스템이름] 로그레벨 메세지
+  return `${timestamp} ${level}: ${message}`; // 날짜 로그레벨: 메세지
 });
 
 /*
  * Log Level
  * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
  */
+
 const logger = winston.createLogger({
   //* 로그 출력 형식 정의
   format: combine(
     timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    label({ label: '곰곰' }), // 어플리케이션 이름
     logFormat // log 출력 포맷
     //? format: combine() 에서 정의한 timestamp와 label 형식값이 logFormat에 들어가서 정의되게 된다. level이나 message는 콘솔에서 자동 정의
   ),
@@ -66,7 +65,6 @@ if (process.env.NODE_ENV !== 'production') {
       format: winston.format.combine(
         winston.format.colorize(), // 색깔 넣어서 출력
         logFormat
-        // winston.format.simple(), // `${info.level}: ${info.message} JSON.stringify({ ...rest })` 포맷으로 출력
       ),
     })
   );
