ca3eb400d447ede9e550610dbd302abdc06b781c
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-09-07 12:23:54 +0900

    https socket

--- app.js
+++ app.js
@@ -73,8 +73,8 @@ app.use(errorHandler);
 //   console.log(port, '포트로 서버가 열렸어요!');
 // });
 
-const server = http.createServer(app).listen(3000);
-https.createServer(options, app).listen(443);
+http.createServer(app).listen(3000);
+const server = https.createServer(options, app).listen(443);
 
 webSocket(server, app);
 //
