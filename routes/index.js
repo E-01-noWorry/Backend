b91b5bcbb98e6d85c04b272801a713e6a3f3d3a7
김대린 <62937325+kimdaerin@users.noreply.github.com>
2022-09-13 10:34:30 +0900

    dd

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const selectRouter = require('./select');
 const userRouter = require('./user');
 const voteRouter = require('./vote');
 const commentRouter = require('./comment');
+const recommentRouter = require('./recomment');
 const chatRouter = require('./chat');
 const myRouter = require('./mypage');
 
@@ -12,6 +13,7 @@ router.use('/select', selectRouter);
 router.use('/', userRouter);
 router.use('/select/vote', voteRouter);
 router.use('/comment', commentRouter);
+router.use('/recomment', recommentRouter);
 router.use('/room', chatRouter);
 router.use('/my', myRouter);
 
