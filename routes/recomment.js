3a2d9f5fc3b54c772a4510db3f6e6c78c8b3a3d4
김대린 <62937325+kimdaerin@users.noreply.github.com>
2022-09-13 15:03:04 +0900

    대댓글

--- routes/recomment.js
+++ routes/recomment.js
@@ -3,156 +3,168 @@ const router = express.Router();
 const { User, Comment, Recomment } = require('../models');
 const authMiddleware = require('../middlewares/authMiddlware');
 const ErrorCustom = require('../advice/errorCustom');
-const { RoboMaker } = require('aws-sdk');
 
 // 대댓글 작성
 router.post('/:commentKey', authMiddleware, async (req, res, next) => {
-    try {
-      const { userKey, nickname } = res.locals.user;
-      const { commentKey } = req.params;
-      const { recomment } = req.body;
+  try {
+    const { userKey, nickname } = res.locals.user;
+    const { commentKey } = req.params;
+    const { comment } = req.body;
+    console.log(comment);
 
-      console.log(recomment);
+    if (comment === '') {
+      throw new ErrorCustom(400, '댓글을 입력해주세요.');
+    }
 
-      if (recomment === '') {
-        throw new ErrorCustom(400, '대댓글을 입력해주세요.');
-      }
-  
-      const data = await Comment.findOne({ where: { commentKey } });
-      console.log(data);
+    const data = await Comment.findOne({ where: { commentKey } });
 
-  
-      if(!data) {
-        throw new ErrorCustom(400, '해당 댓글이 존재하지 않습니다.'); //댓글 확인
-      }
-  
-      const newComment = await Recomment.create({
-        recomment,
-        commentKey,
+    if (!data) {
+      throw new ErrorCustom(400, '해당 댓글이 존재하지 않습니다.');
+    }
+
+    const newComment = await Recomment.create({
+      comment,
+      commentKey,
+      userKey,
+    });
+
+    newComment.updatedAt = newComment.updatedAt.setHours(
+      newComment.updatedAt.getHours() + 9
+    );
+
+    return res.status(200).json({
+      ok: true,
+      msg: '대댓글 작성 성공',
+      result: {
+        recommentKey: newComment.recommentKey,
+        comment: newComment.comment,
+        nickname: nickname,
         userKey,
+        time: newComment.updatedAt,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// 해당 게시물 대댓글 모두 조회
+router.get('/:commentKey', async (req, res, next) => {
+  try {
+    const { commentKey } = req.params;
+
+    const data = await Comment.findOne({
+      where: { commentKey },
+    });
+
+    if (!data) {
+      throw new ErrorCustom(400, '해당 대댓글이 존재하지 않습니다.');
+    }
+
+    const datas = await Recomment.findAll({
+      where: { commentKey },
+      include: [{ model: User, attributes: ['nickname'] }],
+      order: [['recommentKey', 'ASC']],
+    });
+
+    return res.status(200).json({
+      ok: true,
+      msg: '대댓글 조회 성공',
+      result: datas.map((e) => {
+        return {
+          recommentKey: e.recommentKey,
+          comment: e.comment,
+          nickname: e.User.nickname,
+          userKey: e.userKey,
+          time: e.updatedAt,
+        };
+      }),
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// 해당 댓글 수정
+router.put('/:recommentKey', authMiddleware, async (req, res, next) => {
+  try {
+    const { userKey, nickname } = res.locals.user;
+    const { recommentKey } = req.params;
+    const { comment } = req.body;
+
+    if (comment === '') {
+      throw new ErrorCustom(400, '댓글을 입력해주세요.');
+    }
+    // if (comment.length > 200) {
+    // throw new ErrorCustom(400, '댓글은 200자 이내로 작성 가능합니다.');
+    // }
+
+    const data = await Recomment.findOne({
+      where: { recommentKey },
+    });
+
+    if (!data) {
+      throw new ErrorCustom(400, '해당 댓글이 존재하지 않습니다.');
+    }
+
+    if (userKey !== data.userKey) {
+      throw new ErrorCustom(400, '작성자가 다릅니다.');
+    } else {
+      await Recomment.update({ comment }, { where: { recommentKey, userKey } });
+
+      const updateComment = await Recomment.findOne({
+        where: { recommentKey },
       });
 
       return res.status(200).json({
         ok: true,
-        msg: '대댓글 작성 성공',
+        msg: '대댓글 수정 성공',
         result: {
-          recommentKey: newComment.recommentKey,
-          recomment: newComment.recomment,
+          recommentKey: data.recommentKey,
+          comment: comment,
+          nickname: nickname,
+          userKey,
+          time: updateComment.updatedAt,
+        },
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+// 대댓글 삭제
+router.delete('/:recommentKey', authMiddleware, async (req, res, next) => {
+  try {
+    const { userKey, nickname } = res.locals.user;
+    const { recommentKey } = req.params;
+
+    const data = await Recomment.findOne({ where: { recommentKey } });
+
+    if (!data) {
+      throw new ErrorCustom(400, '해당 대댓글이 존재하지 않습니다.');
+    }
+
+    if (userKey !== data.userKey) {
+      throw new ErrorCustom(400, '작성자가 다릅니다.');
+    } else {
+      await Recomment.destroy({ where: { recommentKey, userKey } });
+
+      return res.status(200).json({
+        ok: true,
+        msg: '대댓글 삭제 성공',
+        result: {
+          recommentKey: data.recommentKey,
+          comment: data.comment,
           nickname: nickname,
           userKey,
         },
       });
-  
-    }catch(err) {
-        next(err)
     }
-  });
-  
-  // //대댓글 수정 수정은 그냥 댓글수정과 다를 건 없을 듯
-  // router.put('/:parentKey/:commentKey', async (req,res,next) => {
-  //   try {
-  //     const { userKey, nickname } = res.locals.user;
-  //     const { commentKey } = req.params.commentKey;
-  //     const { comment } = req.body;
-  
-  //     if (comment === '') {
-  //       throw new ErrorCustom(400, '대댓글을 입력해주세요.');
-  //     }
-  
-  //     const data = await Comment.findOne({
-  //       where: { commentKey },
-  //     });
-  
-  //     if (!data) {
-  //       throw new ErrorCustom(400, '해당 대댓글이 존재하지 않습니다.');
-  //     }
-  
-  //     if (userKey !== data.userKey) {
-  //       throw new ErrorCustom(400, '작성자가 다릅니다.');
-  //     } else {
-  //       await Comment.update({ comment }, { where: { commentKey, userKey } });
-  
-  //       const updateComment = await Comment.findOne({
-  //         where: { commentKey },
-  //       });
-  
-  //       return res.status(200).json({
-  //         ok: true,
-  //         msg: '댓글 수정 성공',
-  //         result: {
-  //           commentKey: data.commentKey,
-  //           comment: comment,
-  //           nickname: nickname,
-  //           userKey        
-  //         },
-  //       });
-  //     }
-  //   } catch (err) {
-  //     next(err);
-  //   }
-  // });
-  
-  // //대댓글 삭제 삭제는 그냥 있는 걸 똑같이 쓰면 될거 같으나 대댓글 개수 순서 등을 수정해줘야해서 다를 듯 
-  // router.delete('/:parentKey/:commentKey', authMiddleware, async (req, res, next) => {
-  //   try {
-  //     const { userKey, nickname } = res.locals.user;
-  //     const { commentKey } = req.params;
-  
-  //     const data = await Comment.findOne({ where: { commentKey } });
-  
-  //     if (!data) {
-  //       throw new ErrorCustom(400, '해당 대댓글이 존재하지 않습니다.');
-  //     }
-  
-  //     if (userKey !== data.userKey) {
-  //       throw new ErrorCustom(400, '작성자가 다릅니다.');
-  //     } 
-  
-  //     const count = await Comment.findOne({ //부모의 대댓글 개수
-  //       where: { count: parentKey.count }
-  //     });
-  
-  //   //자식이 없게될 경우
-  //   if(count === 0 ) {
-  //     const data1 = await Comment.update({ //자식이 없으니 0, group 정보도 없앰
-  //         parent: 0,
-  //         group: 0
-  //     });
-  //   }
-  
-  //   //자식있으면 대댓글 길이만 변경
-  //   else{ 
-  //     const data2 = await Comment.update({ //자식이 없으니 0, group 정보도 없앰
-  //       // count =  count - 1, //코드 추후 수정
-  //     });
-  //   }
-  
-  //   //이제 대댓글 정보 수정 대댓글 순서정보 가져오고 
-  //   const commentCount = await Comment.findOne({ //해당 대댓글의 순서 정보
-  //     where: { count: commentKey.order }
-  //   });
-  
-  //   //이제 해당 대댓글의 순서보다 큰 애들만 order -= 1해준다
-  
-  
-  //   //했다면 대댓글을 삭제하면 끝일듯
-  
-  //   await Comment.destroy({ where: { commentKey, userKey } });
-  
-  //   return res.status(200).json({
-  //     ok: true,
-  //     msg: '대댓글 삭제 성공',
-  //     result: {
-  //       commentKey: data.commentKey,
-  //       comment: data.comment,
-  //       nickname: nickname,
-  //       userKey,
-  //     }
-  //   });
-  
-  //   } catch (err) {
-  //     next(err);
-  //   }
-  // });
+  } catch (err) {
+    next(err);
+  }
+});
 
-  module.exports = router;
+
+module.exports = router;
