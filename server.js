de1e1c859d098905c366ede6b9832b47a81bb789
조은지 <107025988+JJooonji@users.noreply.github.com>
2022-09-21 18:14:23 +0900

    포트 수정

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { server } = require('./socket');
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT2;
 
 server.listen(port, () => {
   console.log(port, '포트로 http 서버가 열렸어요!');
