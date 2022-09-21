889482c6a66746e074b0fa88aeff55e4f527364f
JJooonji <korewa9@naver.com>
2022-09-21 18:24:57 +0900

    서버 코드 분리 3차

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { server } = require('./socket');
 require('dotenv').config();
-const port = process.env.PORT;
+const port2 = process.env.PORT;
 
-server.listen(port, () => {
-  console.log(port, '포트로 http 서버가 열렸어요!');
+server.listen(port2, () => {
+  console.log(port2, '포트로 http 서버가 열렸어요!');
 })
