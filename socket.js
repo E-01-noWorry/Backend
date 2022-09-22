610db7bd6acca81f7daec5c078ffab313b468418
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-09-22 12:00:47 +0900

    Update socket.js

--- socket.js
+++ socket.js
@@ -86,7 +86,7 @@ io.on('connection', (socket) => {
 
     // 처음입장이라면 환영 메세지가 없을테니
     if (!enterMsg) {
-      const today = dayjs().format('YYYY-MM-DD 00:00:00');
+      const today = dayjs().format('YYYY-MM-DD hh:mm:ss');
 
       const todayChat = await Chat.findOne({
         where: {
@@ -99,7 +99,7 @@ io.on('connection', (socket) => {
         await Chat.create({
           roomKey,
           userKey: 12, // 관리자 유저키
-          chat: `${dayjs(today).format('YYYY년 MM월 DD일')}`,
+          chat: `${dayjs(today).format('YYYY-MM-DD hh:mm:ss')}`,
         });
       }
 
@@ -121,7 +121,7 @@ io.on('connection', (socket) => {
   socket.on('chat_message', async (data) => {
     let { message, roomKey, userKey } = data;
 
-    const today = dayjs().format('YYYY-MM-DD 00:00:00');
+    const today = dayjs().format('YYYY-MM-DD hh:mm:ss');
 
     const todayChat = await Chat.findOne({
       where: {
@@ -135,7 +135,7 @@ io.on('connection', (socket) => {
       await Chat.create({
         roomKey,
         userKey: 12, // 관리자 유저키
-        chat: `${dayjs(today).format('YYYY년 MM월 DD일')}`,
+        chat: `${dayjs(today).format('YYYY-MM-DD hh:mm:ss')}`,
       });
     }
 
