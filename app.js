4c3f6ea4148cedcabaa8af9a134ed2088e24a0ca
JJooonji <korewa9@naver.com>
2022-09-22 23:22:34 +0900

    주석 코드 삭제

--- app.js
+++ app.js
@@ -3,18 +3,12 @@ const Router = require('./routes/index');
 const errorHandler = require('./advice/errorHandler');
 const logger = require('./config/winston');
 const morganMiddleware = require('./config/morganMiddleware');
-// const fs = require('fs');
-// const http = require('http');
-// const https = require('https');
 
 const session = require('cookie-session');
 const passport = require('passport');
 const passportConfig = require('./passport');
 
-// const webSocket = require('./socket');
-
 require('dotenv').config();
-// const port = process.env.PORT;
 
 const app = express();
 
@@ -57,31 +51,5 @@ app.get('/', (req, res) => {
   res.status(200).json({ massage: '서버 잘 켜짐.' });
 });
 app.use(errorHandler);
-
-// if (process.env.NODE_ENV == 'production') {
-//   try {
-//     const port2 = process.env.PORT2;
-//     const options = {
-//       ca: fs.readFileSync('/etc/letsencrypt/live/jolee.shop/fullchain.pem'),
-//       key: fs.readFileSync('/etc/letsencrypt/live/jolee.shop/privkey.pem'),
-//       cert: fs.readFileSync('/etc/letsencrypt/live/jolee.shop/cert.pem'),
-//     };
-//     app.use(express.static('public'));
-
-//     http.createServer(app).listen(port);
-//     const server = https.createServer(options, app).listen(port2, () => {
-//       console.log(port2, '포트로 https 서버가 열렸어요!');
-//     });
-//     webSocket(server, app);
-//   } catch (err) {
-//     console.log('HTTPS 서버가 실행되지 않습니다.');
-//     console.log(err);
-//   }
-// } else {
-// const server = app.listen(port, () => {
-//   console.log(port, '포트로 http 서버가 열렸어요!');
-// });
-// webSocket(server, app);
-// }
 
 module.exports = app;
