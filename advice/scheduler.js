22e45b8648af9d5a11afce0da48d3bd0955fa48f
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-09-24 02:27:35 +0900

    Update scheduler.js

--- advice/scheduler.js
+++ advice/scheduler.js
@@ -3,12 +3,14 @@ const { Select } = require('../models');
 const dayjs = require('dayjs');
 
 exports.scheduler = () => {
-  const now = dayjs(new Date()).add(9, 'h').format();
-  now = dayjs(now).subtract(16, 'm').format();
-  now = dayjs(now).subtract(59, 's').format();
+  const now = dayjs(new Date())
+    .add(9, 'h')
+    .subtract(16, 'm')
+    .subtract(59, 's')
+    .format();
   console.log(now);
 
-  schedule.scheduleJob('00 25 * * * *', async function () {
+  schedule.scheduleJob('00 29 * * * *', async function () {
     console.log(now);
     console.log('데이터 확인');
     const datas = await Select.findAll({});
