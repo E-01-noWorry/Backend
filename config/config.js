17ec1a661d5d8269fc3070e57405813b68b53ede
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-09-24 17:17:17 +0900

    Update config.js

--- config/config.js
+++ config/config.js
@@ -21,10 +21,16 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    username: process.env.MYSQL_NAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.DATEBASE,
+    host: process.env.HOST,
     dialect: 'mysql',
+    logging: false,
+    timezone: '+09:00',
+    dialectOptions: {
+      dateStrings: true,
+      typeCast: true,
+    },
   },
 };
