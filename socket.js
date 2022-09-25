109ad6ff6142affa3b0ded6e49ddc5f0eb52f588
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-09-26 02:45:04 +0900

    Update socket.js

--- socket.js
+++ socket.js
@@ -78,7 +78,7 @@ io.on('connection', (socket) => {
 
     // 처음입장이라면 환영 메세지가 없을테니
     if (!enterMsg) {
-      const today = dayjs(new Date()).format('YYYY-MM-DD 15:00:00');
+      const today = dayjs(new Date()).add(9, 'h').format('YYYY-MM-DD 15:00:00');
 
       const todayChat = await Chat.findOne({
         where: {
@@ -113,7 +113,7 @@ io.on('connection', (socket) => {
   socket.on('chat_message', async (data) => {
     let { message, roomKey, userKey } = data;
 
-    const today = dayjs(new Date()).format('YYYY-MM-DD 15:00:00'); // new Date().add(9, 'h')'YYYY-MM-DD 00:00:00' 'YYYY-MM-DD hh:mm:ss'
+    const today = dayjs(new Date()).add(9, 'h').format('YYYY-MM-DD 15:00:00'); // new Date().add(9, 'h')'YYYY-MM-DD 00:00:00' 'YYYY-MM-DD hh:mm:ss'
     console.log(today, '00시 기준');
 
     const todayChat = await Chat.findOne({
