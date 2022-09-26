8b0e05f72fbe951f3be549161e7d46f6b3e9e58c
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-09-26 22:27:43 +0900

    댓글, 대댓글 서비스 분리

--- controllers/recomment.controller.js
+++ controllers/recomment.controller.js
@@ -1,78 +1,82 @@
 const ErrorCustom = require('../advice/errorCustom');
 const joi = require('../advice/joiSchema');
 
-const RecommentService = require('../services/recomment.service'); 
-
+const RecommentService = require('../services/recomment.service');
 
 class RecommentController {
-    recommentService = new RecommentService(); 
+  recommentService = new RecommentService();
 
-    postRecomment = async(req,res,next) => {
-        try {
+  postRecomment = async (req, res, next) => {
+    try {
+      const { userKey, nickname } = res.locals.user;
+      const { commentKey } = joi.commentKeySchema.validate(req.params).value;
+      const result = joi.recommentSchema.validate(req.body);
 
-            const { userKey, nickname } = res.locals.user;
-            const { commentKey } = joi.commentKeySchema.validate(req.params).value;
-            const result = joi.recommentSchema.validate(req.body);
-            if (result.error) {
-              throw new ErrorCustom(400, '대댓글을 입력해주세요.');
-            }
-            const { comment } = result.value;
+      if (result.error) {
+        throw new ErrorCustom(400, '대댓글을 입력해주세요.');
+      }
+      const { comment } = result.value;
 
-        
-            const createRecomment = await this.recommentService.createRecomment(
-              userKey, 
-              commentKey, 
-              comment, 
-              nickname
-            );
+      const createRecomment = await this.recommentService.createRecomment(
+        userKey,
+        commentKey,
+        comment,
+        nickname
+      );
 
-
-          
-            res.status(201).json(createRecomment);
-          } catch (err) {
-            next(err);
-          }
+      res.status(201).json(createRecomment);
+    } catch (err) {
+      next(err);
     }
+  };
 
-    putRecomment = async(req,res,next) => {
-        try {
-            const { userKey, nickname } = res.locals.user;
-            const { recommentKey } = joi.recommentKeySchema.validate(req.params).value;
-            const result = joi.recommentSchema.validate(req.body);
-            if (result.error) {
-              throw new ErrorCustom(400, '대댓글을 입력해주세요. 50자까지 가능합니다.');
-            }
-            const { comment } = result.value;
-            
-            const putRecomments = await this.recommentService.putRecomments(
-              userKey, 
-              recommentKey, 
-              comment,
-              nickname
-            );
+  putRecomment = async (req, res, next) => {
+    try {
+      const { userKey, nickname } = res.locals.user;
+      const { recommentKey } = joi.recommentKeySchema.validate(
+        req.params
+      ).value;
+      const result = joi.recommentSchema.validate(req.body);
 
-            res.status(201).json(putRecomments);
-          } catch (err) {
-            next(err);
-          }
+      if (result.error) {
+        throw new ErrorCustom(
+          400,
+          '대댓글을 입력해주세요. 50자까지 가능합니다.'
+        );
+      }
+      const { comment } = result.value;
+
+      const putRecomment = await this.recommentService.putRecomment(
+        userKey,
+        recommentKey,
+        comment,
+        nickname
+      );
+
+      res.status(201).json(putRecomment);
+    } catch (err) {
+      next(err);
     }
+  };
 
-    deleteRecomment = async(req,res,next) => {
-        try {
-            const { userKey, nickname } = res.locals.user;
-            const { recommentKey } = joi.recommentKeySchema.validate(req.params).value;
-        
-            const deleteRecomments = await this.recommentService.deleteRecomments(
-              userKey, 
-              recommentKey,
-              nickname
-            );
+  deleteRecomment = async (req, res, next) => {
+    try {
+      const { userKey, nickname } = res.locals.user;
+      const { recommentKey } = joi.recommentKeySchema.validate(
+        req.params
+      ).value;
 
-            res.status(201).json(deleteRecomments);
-          } catch (err) {
-            next(err);
-          }
+      const deleteRecomment = await this.recommentService.deleteRecomment(
+        userKey,
+        recommentKey,
+        nickname
+      );
+
+      res.status(201).json(deleteRecomment);
+    } catch (err) {
+      next(err);
     }
+  };
 }
 
 module.exports = RecommentController;
