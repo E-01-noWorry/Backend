6e25b03adf32b1f11262fcfb3cb0ed5b7c92d384
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-09-26 12:48:25 +0900

    채팅 날짜 제발 ㅠ

--- advice/scheduler.js
+++ advice/scheduler.js
@@ -1,17 +1,26 @@
 const schedule = require('node-schedule');
 const { Select } = require('../models');
 const dayjs = require('dayjs');
+const timezone = require('dayjs/plugin/timezone');
+const utc = require('dayjs/plugin/utc');
 
 exports.scheduler = () => {
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  dayjs.tz.setDefault('Asia/Seoul');
+
   let now;
   if (process.env.NODE_ENV == 'production' && process.env.PORT2) {
-    now = dayjs(new Date()).add(9, 'h').add(2, 'm').subtract(9, 's').format();
+    // now = dayjs(new Date()).add(9, 'h').add(2, 'm').subtract(9, 's').format();
+    now = dayjs().tz().format();
+    // console.log(now)
   } else {
-    now = dayjs(new Date()).format();
+    // now = dayjs(new Date()).format();
+    now = dayjs().tz().format();
   }
   console.log(now);
 
-  schedule.scheduleJob('00 00 * * * *', async function () {
+  schedule.scheduleJob('*/20 * * * *', async function () {
     console.log(now);
     console.log('데이터 확인');
     const datas = await Select.findAll({});
